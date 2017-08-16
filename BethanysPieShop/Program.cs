a1dedfec083344e96d138b4e3be74fe7d50737cb
gscottanderson <garlandscottanderson@gmail.com>
2017-08-16 09:03:05 -0400

    2.0 changes

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,21 +1,32 @@
-﻿using System.IO;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace BethanysPieShop
 {
     public class Program
     {
+        //public static void Main(string[] args)
+        //{
+        //    var host = new WebHostBuilder()
+        //        .UseKestrel()
+        //        .UseContentRoot(Directory.GetCurrentDirectory())
+        //        .UseIISIntegration()
+        //        .UseStartup<Startup>()
+        //        .UseApplicationInsights()
+        //        .Build();
+
+        //    host.Run();
+        //}
+
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
-
-            host.Run();
+            BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+        
     }
 }
