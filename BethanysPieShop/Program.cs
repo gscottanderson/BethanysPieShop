ade99ac81a643500b60e9670f6e6e9fd54e31acf
gscottanderson <garlandscottanderson@gmail.com>
2017-08-20 17:25:43 -0400

    2.0 changes

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace BethanysPieShop
@@ -26,7 +27,48 @@ namespace BethanysPieShop
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
-        
+
+
+
+        //var builder = new WebHostBuilder()
+        //        .UseKestrel()
+        //        .UseContentRoot(Directory.GetCurrentDirectory())
+        //        .ConfigureAppConfiguration((hostingContext, config) =>
+        //        {
+        //            var env = hostingContext.HostingEnvironment;
+
+        //            config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        //                  .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+        //            if (env.IsDevelopment())
+        //            {
+        //                var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+        //                if (appAssembly != null)
+        //                {
+        //                    config.AddUserSecrets(appAssembly, optional: true);
+        //                }
+        //            }
+
+        //            config.AddEnvironmentVariables();
+
+        //            if (args != null)
+        //            {
+        //                config.AddCommandLine(args);
+        //            }
+        //        })
+        //        .ConfigureLogging((hostingContext, logging) =>
+        //        {
+        //            logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+        //            logging.AddConsole();
+        //            logging.AddDebug();
+        //        })
+        //        .UseIISIntegration()
+        //        .UseDefaultServiceProvider((context, options) =>
+        //        {
+        //            options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+        //        });
+
     }
 }
